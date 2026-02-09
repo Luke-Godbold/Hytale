0ff285e5aa06f96bd334a781498a7bb48339688f
Luke-Godbold <luke.godbold2018@icloud.com>
2026-02-09 15:23:19 +0000

    hi

--- frontend/src/Guides.js
+++ frontend/src/Guides.js
@@ -102,6 +102,14 @@ function GuidesPage() {
     setUpdated(!updated)
   }
 
+  async function scroolToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    
+  }
+
   // runs the fetch guides function when the page is loaded
   useEffect(() => {
     fetchGuides()
@@ -146,6 +154,11 @@ function GuidesPage() {
             </div>
           ))}
           <button className={button2 + ' fixed z-50 bottom-5 left-5'} onClick={() => {setContent([]);}}>Back to Guides</button>
+          <button className={'fixed z-50 bottom-5 right-5 cursor-pointer'} onClick={() => {scroolToTop()}}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+            </svg>
+            <p className='hidden hover:flex'></p>
+          </button>
         </div>}
     </>
   );
